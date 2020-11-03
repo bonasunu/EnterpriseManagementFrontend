e3c52d7b32e70ea770ddcd20b1aa65c790acb588
bonasunu <bonaventurasunu@gmail.com>
2020-11-03 23:55:19 +0700

    stling title on Content

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Content = () => {
   const classes = {
-    wrapper: 'flex-grow overflow-y-auto bg-orange-100',
-    data: 'm-5 bg-white h-screen',
+    wrapper: 'flex-grow overflow-y-auto bg-orange-100 px-5',
+    pageTitle: 'm-8 text-gray-600 font-bold text-xl',
+    data: 'm-8 bg-white h-screen',
   }
 
   return (
     <div className={classes.wrapper}>
-      <h1>Content</h1>
+      <h2 className={classes.pageTitle}>Content</h2>
       <div className={classes.data}>Data</div>
     </div>
   )

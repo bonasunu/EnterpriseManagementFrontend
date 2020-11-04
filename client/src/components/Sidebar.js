80fe3999480a8ce3afa54aa03b9d74406fce88ce
bonasunu <bonaventurasunu@gmail.com>
2020-11-04 19:02:41 +0700

    fix title on SIdebar components

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -5,7 +5,7 @@ const Sidebar = () => {
     wrapper:
       'bg-white hidden sm:hidden md:hidden lg:inline xl:inline xl:col-span-1 lg:col-span-1',
     title:
-      'text-red-500 font-extrabold text-center text-2xl w-full mt-5 mb-5 p-3',
+      'text-red-500 font-extrabold text-center text-2xl md:text-xl w-full mt-5 mb-5 px-5',
     linkWrapper: 'w-full text-left flex flex-col',
     link:
       'mt-1 mb-1 px-6 py-3 text-lg font-semibold w-full h-12 hover:bg-orange-200 hover:text-gray-800 text-gray-600',

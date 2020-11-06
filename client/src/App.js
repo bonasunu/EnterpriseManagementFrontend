b353f6c6a95821594c7a9cc0c26b19845e01c0fe
bonasunu <bonaventurasunu@gmail.com>
2020-11-06 13:16:17 +0700

    styling main content

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const classes = {
     wrapper:
       'overflow-x-hidden h-screen grid grid-cols-1 lg:grid-cols-6 sm:grid-cols-1 md:grid-cols-1',
-    main: 'overflow-y-auto flex flex-col lg:col-span-5',
+    main: 'overflow-y-auto h-auto flex flex-col lg:col-span-5',
     sidebarWeb:
       'bg-white hidden sm:hidden md:hidden lg:inline xl:inline xl:col-span-1 lg:col-span-1',
     sidebarMobile: 'sticky bg-white lg:hidden xl:hidden',
--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -66,13 +66,14 @@ const options = {
 
 const Content = () => {
   const classes = {
-    wrapper: 'flex-grow overflow-y-auto overflow-x-hidden bg-orange-100',
-    pageTitle:
-      'mt-8 ml-8 mb-1 sm:ml-1 md:ml-2 lg:ml-5 xl:ml-8 lg:px-4 xl:px-2 px-2 text-gray-600 font-bold text-xl',
+    wrapper: 'flex-grow overflow-x-auto bg-orange-100',
+    pageTitle: 'mt-8 ml-8 mb-1 text-gray-600 font-bold text-xl',
     dataWrapper:
-      'flex flex-wrap ml-5 mr-5 sm:ml-1 md:ml-2 lg:ml-5 xl:ml-5 h-64 sm:h-auto md:h-auto lg:h-64 xl:h-64',
+      'flex flex-wrap ml-8 mr-8 h-64 sm:h-auto md:h-auto lg:h-64 xl:h-64',
     data:
-      'flex-grow mr-3 ml-3 mt-5 mb-5 sm:ml-2 md:ml-2 lg:ml-5 xl:ml-5 bg-white overflow-x-auto h-64 lg:w-1/4 sm:w-auto sm:h-auto md:w-auto py-8 px-8 sm:px-2 rounded-md',
+      'flex-grow mr-5 mt-5 mb-3 bg-white overflow-x-auto h-64 lg:w-1/4 sm:w-auto sm:h-auto md:w-auto py-8 px-5 sm:px-2 lg:px-5 xl:px-5 rounded-md',
+    table: 'table-auto w-full',
+    tableData: 'border px-4 py-2',
   }
 
   return (
@@ -90,6 +91,69 @@ const Content = () => {
         </div>
         <div className={classes.data}>
           <Line data={dataLine} options={options} />
+        </div>
+        <div className={classes.data}>
+          <table className={classes.table}>
+            <thead>
+              <tr>
+                <th className={classes.tableData}>Project</th>
+                <th className={classes.tableData}>Developer</th>
+                <th className={classes.tableData}>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className={classes.tableData}>Frontend</td>
+                <td className={classes.tableData}>John Doe</td>
+                <td className={classes.tableData}>On progress</td>
+              </tr>
+              <tr>
+                <td className={classes.tableData}>Frontend</td>
+                <td className={classes.tableData}>John Doe</td>
+                <td className={classes.tableData}>On progress</td>
+              </tr>
+              <tr>
+                <td className={classes.tableData}>Frontend</td>
+                <td className={classes.tableData}>John Doe</td>
+                <td className={classes.tableData}>On progress</td>
+              </tr>
+              <tr>
+                <td className={classes.tableData}>Frontend</td>
+                <td className={classes.tableData}>John Doe</td>
+                <td className={classes.tableData}>On progress</td>
+              </tr>
+              <tr>
+                <td className={classes.tableData}>Frontend</td>
+                <td className={classes.tableData}>John Doe</td>
+                <td className={classes.tableData}>On progress</td>
+              </tr>
+              <tr>
+                <td className={classes.tableData}>Frontend</td>
+                <td className={classes.tableData}>John Doe</td>
+                <td className={classes.tableData}>On progress</td>
+              </tr>
+              <tr>
+                <td className={classes.tableData}>Frontend</td>
+                <td className={classes.tableData}>John Doe</td>
+                <td className={classes.tableData}>On progress</td>
+              </tr>
+              <tr>
+                <td className={classes.tableData}>Frontend</td>
+                <td className={classes.tableData}>John Doe</td>
+                <td className={classes.tableData}>On progress</td>
+              </tr>
+              <tr>
+                <td className={classes.tableData}>Frontend</td>
+                <td className={classes.tableData}>John Doe</td>
+                <td className={classes.tableData}>On progress</td>
+              </tr>
+              <tr>
+                <td className={classes.tableData}>Frontend</td>
+                <td className={classes.tableData}>John Doe</td>
+                <td className={classes.tableData}>On progress</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
